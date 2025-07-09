b3de5d5a33549679b09f0dfbf14ad38e973f2a1f
Thiago Ueda <thiagokoiti@gmail.com>
2025-07-09 14:39:42 -0300

    implementação e exibição do menu

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,26 @@
 package br.com.alura.literalura;
 
+import br.com.alura.literalura.view.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private Menu menu;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// A aplicação agora delega toda a execução para a classe Menu
+		menu.exibir();
+
+	}
 }
---
+++ src/main/java/br/com/alura/literalura/view/Menu.java
@@ -0,0 +1,49 @@
+package br.com.alura.literalura.view;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component // Indica ao Spring para gerenciar esta classe
+public class Menu {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void exibir() {
+        var opcao = -1;
+        while(opcao != 0) {
+            var menuTexto = """
+                     ***********************************
+                     Escolha o número de sua opção:
+                     1- Buscar livro pelo título
+                     2- Listar livros registrados
+                     3- Listar autores registrados
+                     4- Listar autores vivos em um determinado ano
+                     5- Listar livros em um determinado idioma
+                     
+                     0 - Sair
+                     ***********************************
+                    """;
+            System.out.println(menuTexto);
+
+            // Tratamento de exceção para entrada não numérica
+            if(scanner.hasNextInt()) {
+                opcao = scanner.nextInt();
+                scanner.nextLine(); // Limpa o buffer do scanner
+
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Opção 1 escolhida: Buscar livro pelo título");
+                        break;
+                    case 0:
+                        System.out.println("Saindo do LiterAlura...");
+                        break;
+                    default:
+                        System.out.println("Opção Inválida!");
+                }
+            } else {
+                System.out.println("Por favor, digite um número válido");
+                scanner.next(); // Descarta a entrada inválida
+            }
+        }
+    }
+}

638b8eb96c01bdcf98c40cb7b18ca35c991c3314
Thiago Ueda <thiagokoiti@gmail.com>
2025-07-10 21:41:15 -0300

    implementa na package service as classes ConsumoApi, ConverteDados e IConverteDados

---
+++ src/main/java/br/com/alura/literalura/service/ConsumoApi.java
@@ -0,0 +1,30 @@
+package br.com.alura.literalura.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class ConsumoApi {
+
+    public String obterDados(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Erro ao obter dados da API: " + e.getMessage());
+        }
+
+        return response.body();
+    }
+}
+
